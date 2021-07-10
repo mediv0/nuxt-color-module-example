0e58e9b407f15895d7f1eb0bfc856b5a4f318ea8
mediv0 <anonymousaxis@gmail.com>
2021-07-10 14:54:14 +0430

    feat: add sidebar

---
+++ components/sideNavBarUserCounter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="user_count">
+        <div class="user_count__label">
+            <img src="~/assets/images/wi-fi.svg" alt="online users" class="noselect">
+            <p>Online</p>
+        </div>
+        <p class="font-weight-medium">+19</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.user_count {
+    --border-radius-size: 11px;
+    width: 100%;
+    background: var(--bg-secondary);
+    padding: 9px 10px;
+    border-radius: var(--border-radius-size);
+    font-size: 0.8rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &__label {
+        display: flex;
+        align-items: center;
+        border: 1px solid var(--bg-very-light);
+        border-radius: var(--border-radius-size);
+        padding: 6px 10px;
+
+        & > img {
+            margin-right: 5px;
+        }
+    }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        <Nuxt />
+    <div class="main_layout layoutContainer">
+        <side-nav-bar />
+        <Nuxt class="main_layout__content" />
     </div>
 </template>
+
+<script>
+import sideNavBar from "@/components/sideNavBar.vue";
+export default {
+    components: {
+        sideNavBar
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.main_layout {
+    display: flex;
+    align-items: stretch;
+    height: 100vh;
+
+    &__content {
+        flex: 1;
+    }
+}
+</style>
