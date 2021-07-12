00e10f8784e405a39e58880b46c42efb0ce305c3
mediv0 <anonymousaxis@gmail.com>
2021-07-12 12:08:49 +0430

    feat: update main content layout size

---
+++ pages/app/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="main_app">
+        <stream-list />
+    </div>
+</template>
+
+<script>
+import youtubeStreamCategory from "~/components/pages/app/youtubeStreamCategory.vue";
+export default {
+    components: {
+        streamList: youtubeStreamCategory,
+    },
+    layout: "default",
+};
+</script>
+
+<style lang="scss" scoped>
+.main_app {
+    padding-right: 61px;
+}
+</style>
