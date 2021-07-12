2913aa971d0d4957625b63d4e7f25521f951cd34
mediv0 <anonymousaxis@gmail.com>
2021-07-12 17:26:24 +0430

    feat: add theater hero call to action

---
+++ components/pages/app/theaterHero.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="theater_hero">
+        <div class="theater_hero__img">
+            <img src="@/assets/images/movieHero.png" alt="watch movies with friends" class="fit-parent">
+        </div>
+        <div class="theater_hero__content">
+            <h1>
+                <p>Watch</p>
+                <p>streaming</p>
+                <p>media with</p>
+                <p>friends.</p>
+            </h1>
+            <button class="btn btn-primary">Create Room</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.theater_hero {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 30px 10px 20px;
+
+    &__img {
+        width: 623px;
+        height: 300px;
+        background: var(--bg-primary);
+        border-radius: 40px;
+        overflow: hidden;
+    }
+
+    &__content {
+        & > h1 {
+            font-weight: 500;
+            font-size: 2.6rem;
+            line-height: 41px;
+        }
+
+        & > button {
+            margin-top: 22px;
+            width: 142px
+        }
+    }
+}
+</style>
--- pages/app/index.vue
+++ pages/app/index.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="main_app">
         <stream-list />
+        <theaterHero />
     </div>
 </template>
 
 <script>
 import youtubeStreamCategory from "~/components/pages/app/youtubeStreamCategory.vue";
+import theaterHero from "~/components/pages/app/theaterHero.vue";
 export default {
     components: {
         streamList: youtubeStreamCategory,
+        theaterHero
     },
     layout: "default",
 };
