a5b8f4d342ed981b17fcea43ca16cdb83a08d6d3
mediv0 <anonymousaxis@gmail.com>
2021-07-13 20:22:33 +0430

    chore: add cat pages

---
+++ pages/app/category/_cat.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="single_category">file _cat {{ this.cat }}</div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log(this);
+    }
+}
+</script>
---
+++ pages/app/category/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="stream_category">salam</div>
+</template>
+
+<script>
+export default {};
+</script>
+
