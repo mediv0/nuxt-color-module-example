72cb3175afc7662b5fd4f81b66db86c0952f99b1
mediv0 <anonymousaxis@gmail.com>
2021-07-13 20:34:49 +0430

    chore: move app to /

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,27 @@
 <template>
-    <welcome />
+    <div class="main_app">
+        <stream-list />
+        <theaterHero />
+        <tracks />
+    </div>
 </template>
 
 <script>
-import welcome from "@/components/welcome.vue";
+import youtubeStreamCategory from "~/components/pages/app/youtubeStreamCategory.vue";
+import theaterHero from "~/components/pages/app/theaterHero.vue";
+import tracks from "~/components/tracks/tracks.vue";
 export default {
-    // check if it's first login
-    // if not redirect to player
     components: {
-        welcome,
+        streamList: youtubeStreamCategory,
+        theaterHero,
+        tracks,
     },
-    layout: "noHeaders",
+    layout: "default",
 };
 </script>
+
+<style lang="scss" scoped>
+.main_app {
+    padding-right: 61px;
+}
+</style>
