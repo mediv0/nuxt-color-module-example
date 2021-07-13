b2df2a2b73e337613491c48931031c896f21f879
mediv0 <anonymousaxis@gmail.com>
2021-07-13 20:41:50 +0430

    cd7deaa

---
+++ _nuxt/8ef71f0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{305:function(t,n,e){"use strict";e.r(n);var c={mounted:function(){console.log(this)}},l=e(7),component=Object(l.a)(c,(function(){var t=this,n=t.$createElement;return(t._self._c||n)("div",{staticClass:"single_category"},[t._v("file _cat "+t._s(this.cat))])}),[],!1,null,null,null);n.default=component.exports}}]);
